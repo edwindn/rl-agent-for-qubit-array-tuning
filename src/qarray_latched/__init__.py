66c706b45f257e6e82022ef4710adb636839a554
Edwin De Nicolo <edn@oums-dlgpu1>
2026-01-25 15:33:39 +0000

    fixed error

--- src/qarray_latched/__init__.py
+++ src/qarray_latched/__init__.py
@@ -4,11 +4,11 @@ Date: 31/03/2025
 """
 
 # from .DotArrays.ChargeSensedDotArray import ChargeSensedDotArray  # Empty file
-from .DotArrays.AdvancedLatching import AdvancedLatching
+# from .DotArrays.AdvancedLatching import AdvancedLatching
 from .DotArrays.TunnelCoupledChargeSensed import TunnelCoupledChargeSensed
 
 __all__ = [
     # 'ChargeSensedDotArray',  # Empty file
-    'AdvancedLatching', 
+    # 'AdvancedLatching', 
     'TunnelCoupledChargeSensed'
 ]
